aff1c7665866f7ff1d86d46221c65eb33321fa27
Toomore Chiang <toomore0929@gmail.com>
2022-11-15 17:07:45 +0800

    setup babel config
    Signed-off-by: Toomore Chiang <toomore0929@gmail.com>

--- application.py
+++ application.py
@@ -1,7 +1,16 @@
-from flask import Flask
+from flask import Flask, g
+from flask_babel import Babel
 
 from app import create_app
 
 application = Flask("app")
 
 create_app(application)
+
+babel = Babel(application)
+babel.app.config["BABEL_TRANSLATION_DIRECTORIES"] = "locales"
+
+
+@babel.localeselector
+def get_locale():
+    return getattr(g, "LOCALE", "zh_Hant_TW")
