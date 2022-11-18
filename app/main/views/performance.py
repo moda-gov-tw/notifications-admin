a28f713b82cf7be687eb82ad2b60c78edaefad6f
Toomore Chiang <toomore0929@gmail.com>
2022-11-18 22:44:00 +0800

    Add in views/performance
     - templates/views/performance.html

--- app/main/views/performance.py
+++ app/main/views/performance.py
@@ -4,6 +4,7 @@ from operator import itemgetter
 from statistics import mean
 
 from flask import render_template
+from flask_babel import _
 
 from app import performance_dashboard_api_client, status_api_client
 from app.main import main
@@ -18,7 +19,7 @@ def performance():
     stats["organisations_using_notify"] = sorted(
         [
             {
-                "organisation_name": organisation_name or "No organisation",
+                "organisation_name": organisation_name or _("No organisation"),
                 "count_of_live_services": len(list(group)),
             }
             for organisation_name, group in groupby(
